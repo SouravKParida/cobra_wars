b08d829d39c6e3da3cbeeba604b01ac299fd5d11
Sourav Parida <91864828+SouravKParida@users.noreply.github.com>
2024-12-12 23:39:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Author: Sourav Kumar Parida, Rishi Dharmeshbhai Patel
+# Date: 12/12/2024
+# Description: The program initializes and executes the Cobra Wars game session using the MatchController class.
+
+from matchcontroller import *
+
+if __name__ == "__main__":
+    battleArena = MatchController()
+    battleArena.execute()
